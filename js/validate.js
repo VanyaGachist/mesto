dbfbbd3e235a1f5d66a86f63a1d31032298fc268
Ivan <vany4golos@yandex.ru>
2023-08-02 16:16:34 +0300

    impove validate.js

--- js/validate.js
+++ js/validate.js
@@ -1,55 +1,55 @@
 function enableValidation (config) {
-  const showInputError = (form, input) => {
-    input.classList.add(config.inputErrorClass);
-    const span = form.querySelector('.popup__error-' + input.id);
-    span.textContent = input.validationMessage;
-    span.classList.add(config.errorClass);
-  }
-
-  const hideInputError = (form, input) => {
-    input.classList.remove(config.inputErrorClass);
-    const span = form.querySelector('.popup__error-' + input.id);
-    span.classList.remove(config.errorClass);
-  }
-
-  const hasInvalidValue = (inputs) => {
-    return inputs.some(input => !input.validity.valid);
-  }
-
-  const toogleButtonState = (inputs, button) => {
-    if(hasInvalidValue(inputs)) {
-      button.classList.add(config.inactiveButtonClass);
-      button.disabled = true;
-    } else {
-      button.classList.remove(config.inactiveButtonClass);
-      button.disabled = false;
-    }
-  }
-
-  const isValid = (form, input) => {
-    if (!input.validity.valid) {
-      showInputError(form, input);
-    } else {
-      hideInputError(form, input);
-    }
-  };
-
-  const setEventListeners = (form) => {
-    const inputs = Array.from(form.querySelectorAll(config.inputSelector));
-    const button = form.querySelector(config.submitButtonSelector);
-
-    toogleButtonState(inputs, button);
-
-    inputs.forEach(input => {
-      input.addEventListener('input', () => {
-        isValid(form, input);
-        toogleButtonState(inputs, button);
-      });
-    });
-  }
-
   const forms = Array.from(document.querySelectorAll(config.formSelector));
   forms.forEach(form => {
-    setEventListeners(form);
+    setEventListeners(form, config);
+  });
+}
+
+function showInputError(form, input, config) {
+  input.classList.add(config.inputErrorClass);
+  const span = form.querySelector('.popup__error-' + input.id);
+  span.textContent = input.validationMessage;
+  span.classList.add(config.errorClass);
+}
+
+function hideInputError(form, input, config) {
+  input.classList.remove(config.inputErrorClass);
+  const span = form.querySelector('.popup__error-' + input.id);
+  span.classList.remove(config.errorClass);
+}
+
+function hasInvalidValue(inputs) {
+  return inputs.some(input => !input.validity.valid);
+}
+
+function toogleButtonState(inputs, button, config)  {
+  if(hasInvalidValue(inputs)) {
+    button.classList.add(config.inactiveButtonClass);
+    button.disabled = true;
+  } else {
+    button.classList.remove(config.inactiveButtonClass);
+    button.disabled = false;
+  }
+}
+
+function isValid(form, input, config) {
+  if (!input.validity.valid) {
+    showInputError(form, input, config);
+  } else {
+    hideInputError(form, input, config);
+  }
+};
+
+function setEventListeners(form, config) {
+  const inputs = Array.from(form.querySelectorAll(config.inputSelector));
+  const button = form.querySelector(config.submitButtonSelector);
+
+  toogleButtonState(inputs, button, config);
+
+  inputs.forEach(input => {
+    input.addEventListener('input', () => {
+      isValid(form, input, config);
+      toogleButtonState(inputs, button, config);
+    });
   });
 }
