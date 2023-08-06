368700e3274dbaf55464043ec9c61baefd43a9f8
Ivan <vany4golos@yandex.ru>
2023-08-06 15:03:42 +0300

    add initialCards.js

--- js/index.js
+++ js/index.js
@@ -20,28 +20,6 @@ const closePopupWithFullScreanImage = document.querySelector('.popup__close_thir
 const cardsTemplate = document.querySelector('#card__template').content;
 const createPopupButtonCard = document.querySelector('.popup__create');
 
-const initialCards = [
-  {
-    name: 'Mr. Vselenaya',
-    link: 'https://i.ytimg.com/vi/TVB8pbuUpMA/maxresdefault.jpg'
-  },
-  {
-    name: 'Pepe',
-    link: 'https://phonoteka.org/uploads/posts/2021-06/thumbs/1624840864_1-phonoteka_org-p-lyagushka-pepe-oboi-krasivo-1.jpg'
-  },
-  {
-    name: 'Собчак',
-    link: 'https://wikiwarriors.org/mediawiki/images/e/e4/WalterSobchak.jpg'
-  },
-  {
-    name: '40',
-    link: 'https://cs12.pikabu.ru/post_img/big/2022/01/25/7/1643108970148553768.jpg'
-  },
-  {
-    name: 'bububub',
-    link: 'https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/03/The-boys-billy-butcher-1.jpg'
-  }
-];
 
 function closePopupWithEscape(evt) {
   const opnPopup = document.querySelector('.popup_opened');
