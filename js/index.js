ccd1389b51499c61bbf4e749c0aa633f74877984
Ivan <vany4golos@yandex.ru>
2023-08-26 16:41:41 +0300

    impr valid files

--- js/index.js
+++ js/index.js
@@ -2,38 +2,39 @@ import Card from "./card.js";
 import initialCards from "./initialCards.js";
 import PopupAdd from "./popupAdd.js";
 import PopupEdit from "./popupEdit.js";
-import Validate from "./validateForm.js";
+import FormValidator from "./FormValidator.js";
 import validationConfig from "./validationConfig.js";
 
 const openEditMenuForProfile = document.querySelector('.profile__edit-button');
 const closeEditMenuButton = document.querySelector('.popup__close_first');
 const closeAddMenuButton = document.querySelector('.popup__close_second');
-const editMenuPopup = document.querySelector('.popup_edit');
-const addMenuPopup = document.querySelector('.popup_add');
 const formElementForEditMenu = document.querySelector('.popup__form_type_with-name-and-job');
 const openAddMenuButton = document.querySelector('.profile__add-button');
 const elementsContainer = document.querySelector('.element');
 const formElementForAddMenu = document.querySelector('.popup__form_type_with-image');
 const createPopupButtonCard = document.querySelector('.popup__create');
-const editPopup = new PopupEdit(editMenuPopup);
-const addPopup = new PopupAdd(addMenuPopup);
-const validate = new Validate(validationConfig);
+const editPopup = new PopupEdit('.popup_edit');
+const addPopup = new PopupAdd('.popup_add');
+const validEdit = new FormValidator(validationConfig, '.popup__form_type_with-name-and-job');
+const validAdd = new FormValidator(validationConfig, '.popup__form_type_with-image');
 
-openEditMenuForProfile.addEventListener('click', function () {
-  editPopup.open();
+
+openEditMenuForProfile.addEventListener('click', () => {
+  editPopup.openEditPopup();
+  validEdit.disableSubmitButton();
 });
 
 openAddMenuButton.addEventListener('click', function () {
   addPopup.open();
-  validate.disableSubmitButton(createPopupButtonCard);
+  validAdd.disableSubmitButton();
 });
 
 closeEditMenuButton.addEventListener('click', function () {
-  editPopup.closePopup();
+  editPopup.close();
 });
 
 closeAddMenuButton.addEventListener('click', function () {
-  addPopup.closePopup();
+  addPopup.close();
 });
 
 formElementForEditMenu.addEventListener('submit', (evt) => {
@@ -42,14 +43,17 @@ formElementForEditMenu.addEventListener('submit', (evt) => {
 });
 
 initialCards.forEach(cards => {
-  const card = new Card(cards.name, cards.link);
+  const card = new Card(cards.name, cards.link, '#card__template');
   elementsContainer.append(card.getView());
 });
 
 formElementForAddMenu.addEventListener('submit', (evt) => {
   evt.preventDefault();
   addPopup.addCardsSubmit();
-  addPopup.closePopup();
+  addPopup.close();
 });
 
-validate.enableValidation();
+validEdit.enableValidation();
+validAdd.enableValidation();
+
+
