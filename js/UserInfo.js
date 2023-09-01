3dec18cd0193016a873804678bf9811266b27430
Ivan <vany4golos@yandex.ru>
2023-09-01 16:44:36 +0300

    userInfo class

---
+++ js/UserInfo.js
@@ -0,0 +1,20 @@
+class UserInfo {
+  constructor({ profileTitle, profileDescription }) {
+    this._title = document.querySelector(profileTitle);
+    this._description = document.querySelector(profileDescription);
+  }
+
+  setUserInfo({ name, jobName }) {
+    this._title.textContent = name;
+    this._description.textContent = jobName;
+  }
+
+  getUserInfo() {
+    return {
+      name: this._title,
+      jobName: this._jobName
+    };
+  }
+}
+
+export default UserInfo;
