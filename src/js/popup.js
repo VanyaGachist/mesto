f630912fb972d108d63cc9abe9282d0840da9733
Ivan <vany4golos@yandex.ru>
2023-09-03 15:20:45 +0300

    setEvent to popup

--- src/js/popup.js
+++ src/js/popup.js
@@ -4,20 +4,16 @@ class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
     this._handleClosePopupWithEsc = this._handleClosePopupWithEsc.bind(this);
-    this._handeClosePopupWithClickToZone = this._handeClosePopupWithClickToZone.bind(this);
-    this._closeButton = this._popup.querySelector('.popup__close');
   }
 
   open() {
     this._popup.classList.add('popup_opened');
     window.addEventListener('keydown', this._handleClosePopupWithEsc);
-    this._popup.addEventListener('click', this._handeClosePopupWithClickToZone);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
     window.removeEventListener('keydown', this._handleClosePopupWithEsc);
-    this._popup.removeEventListener('click', this._handeClosePopupWithClickToZone);
   }
 
   _handleClosePopupWithEsc(evt) {
@@ -33,8 +29,13 @@ class Popup {
   }
 
   setEventListeners() {
-    this._closeButton.addEventListener('click', this.close.bind(this));
+    this._popup.querySelector('.popup__close').addEventListener('click', () => {
+      this.close();
+    });
+
+    this._popup.addEventListener('click', this._handeClosePopupWithClickToZone.bind(this));
   }
+
 }
 
 
