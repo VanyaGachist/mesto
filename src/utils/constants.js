eb8c0ba5454237fe8176b4be1872fc8c82b9f223
Ivan <vany4golos@yandex.ru>
2023-09-27 14:58:39 +0300

    remove cards

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,29 +7,6 @@ export const validationConfig = {
   errorClass: 'popup__error_visible'
 };
 
-export const initialCards = [
-  {
-    name: 'Mr. Vselenaya',
-    link: 'https://i.ytimg.com/vi/TVB8pbuUpMA/maxresdefault.jpg'
-  },
-  {
-    name: 'Pepe',
-    link: 'https://phonoteka.org/uploads/posts/2021-06/thumbs/1624840864_1-phonoteka_org-p-lyagushka-pepe-oboi-krasivo-1.jpg'
-  },
-  {
-    name: 'Собчак',
-    link: 'https://wikiwarriors.org/mediawiki/images/e/e4/WalterSobchak.jpg'
-  },
-  {
-    name: '40',
-    link: 'https://cs12.pikabu.ru/post_img/big/2022/01/25/7/1643108970148553768.jpg'
-  },
-  {
-    name: 'bububub',
-    link: 'https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/03/The-boys-billy-butcher-1.jpg'
-  }
-];
-
 export const openEditMenuForProfile = document.querySelector('.profile__edit-button');
 export const openAddMenuButton = document.querySelector('.profile__add-button');
 export const nameImage = document.querySelector('.popup__input_text_name-image');
@@ -38,4 +15,3 @@ export const nameInput = document.querySelector('.popup__input_text_name');
 export const jobInput = document.querySelector('.popup__input_text_after');
 export const openAvatarMenuButton = document.querySelector('.profile__pencil');
 export const avatarInput = document.querySelector('.popup__input_href-avatar');
-export const trashButton = document.querySelector('.element__trash');
